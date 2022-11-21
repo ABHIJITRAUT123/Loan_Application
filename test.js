bb0ce9361b7d4f0aeb08b5900271a777687a1478
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-11-22 01:29:18 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,21 @@ let confirm_content = document.getElementById("confirm-content");
 
 confirm_content.appendChild(deatil);
 
+submit.addEventListener('click',(e)=>{
+    e.preventDefault();
+    validation();
+})
+
+function validation()
+{
+  let valid = document.getElementById("valid").value;
+  
+  if(valid == otp)
+  {
+    let vaildMessage = document.createElement("div").innerText = "Validation Successful!";
+  }
+  else
+  {
+    let invalidMessage = document.createElement("div").innerText = "Validation Failed!";
+  }
+}
