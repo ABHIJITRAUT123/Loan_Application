8fdcdbf1dd740a98238ec8e17086e4b108099688
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-11-23 14:32:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
                 // EMI Calcuation
 
 let loanAmount = document.getElementById("amount");
-// let interestRate = document.getElementById("interest");
-// let loanDuration = document.getElementById("loanTenure");
+let interestRate = document.getElementById("interest");
+let loanDuration = document.getElementById("loanTenure");
 let submit = document.getElementById("calculate");
-
-
 
 submit.addEventListener('click',(e)=>{
     e.preventDefault();
@@ -39,24 +37,21 @@ function myFun(){
 
 function calculateEMI(){
     // First calculate total number of months in loan tenure if selected year
-//     let isYear = document.getElementById("year").checked;
-//     let isMonth = document.getElementById("month").checked;
-//     let noOfMonths=0;
-//     if(isYear=="" && isMonth==""){
-//         alert("Please select loan tenure type-> Month or year");
-//     }else{
-//         if(isYear==true){
-//             noOfMonths=loanDuration.value * 12 ;
-//         }else{
-//             noOfMonths=loanDuration.value;
-//         }
-//         let r = parseFloat(interestRate.value)/12/100;
-//         let P = loanAmount.value;
-//         let n = noOfMonths;
-        
-        let r = parseFloat(8.5)/12/100;
-        let P = loanAmount.value;
-        let n = 15;
+      let isYear = document.getElementById("year").checked;
+      let isMonth = document.getElementById("month").checked;
+      let noOfMonths=0;
+      if(isYear=="" && isMonth==""){
+          alert("Please select loan tenure type-> Month or year");
+      }else{
+          if(isYear==true){
+              noOfMonths=loanDuration.value * 12 ;
+          }else{
+              noOfMonths=loanDuration.value;
+          }
+          let r = parseFloat(interestRate.value)/12/100;
+          let P = loanAmount.value;
+          let n = noOfMonths;
+ 
         //formula EMI= (P * r * (1 + r)^n ) / ((1+r)^n - 1)
         let EMI = (P*r* Math.pow((1+r),n)) / (Math.pow((1+r),n)-1);
         let totalInterest =(EMI * n) - P;
@@ -64,7 +59,7 @@ function calculateEMI(){
         document.getElementById("emi").innerText = Math.ceil(EMI);
         document.getElementById("totalInterest").innerText= Math.ceil(totalInterest);
         document.getElementById("totalPayment").innerText= Math.ceil(totalPayment);
-//     }
+    }
 }
 
 
